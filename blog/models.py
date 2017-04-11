52f9e6c4cc0be28e68df24cdb74dcfdd6a52dd17
dacatay <daniel.acatay@googlemail.com>
2017-04-11 21:28:18 +0200

    added function slugification for blog post detail urls

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
+from autoslug import AutoSlugField
 
 
 # This is the blog apps database
 # each class resembles a table
 # each variable within class resembles a column
 class Post(models.Model):
-    title = models.CharField(max_length=255, default='Title')
+    title = models.CharField(max_length=120, default='Title')
     author = models.CharField(max_length=55, default='Author')
-    slug = models.SlugField(default='Slug')
+    slug = AutoSlugField(populate_from='title', unique=True)
 
     body = models.TextField()
     date = models.DateTimeField('date published')
@@ -15,5 +17,17 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    # def _get_unique_slug(self):
+    #     slug = slugify(self.title)
+    #     unique_slug = slug
+    #     num = 1
+    #     while Post.objects.filter(slug=unique_slug).exists():
+    #         unique_slug = '{}-{}'.format(slug, num)
+    #         num += 1
+    #     return unique_slug
+    #
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = self._get_unique_slug()
+    #     super().save()
 
--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ from blog.models import Post
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by('-date'),
                                 template_name='blog/blog.html')),
-    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Post, template_name='blog/post.html')),
+    url(r'^(?P<slug>[\w-]+)/$', DetailView.as_view(model=Post, template_name='blog/post.html'), name='post'),
 ]
