d25c4189128a21f6dad8c373b9ea64aab47b8a89
dacatay <daniel.acatay@googlemail.com>
2017-04-15 13:36:18 +0200

    enhanced the detail view for related content

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from taggit.models import Tag
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -71,9 +72,16 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/detail.html', {'post': post,
                                                 'comments': comments,
-                                                'comment_form': comment_form})
+                                                'comment_form': comment_form,
+                                                'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
